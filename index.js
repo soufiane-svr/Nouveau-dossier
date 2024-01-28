052a7e9857d3a66032ef23b816f457ba90554e72
soufiane-svr <nwigashop@gmail.com>
2024-01-28 14:42:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const App = express()
+
+App.use(express.json())
+App.get('/send',(req,res)=>{
+
+    res.send(`  your name is   ${req.body.name} and your age is  ${req.query.age}`)
+
+})
+App.get('/',(req,res)=>{
+
+    res.send(`  your name is   ${req.body.name} and your age is  ${req.query.age}`)
+
+})
+
+
+
+App.listen(3000, ()=>{
+    console.log('console : ');
+})
